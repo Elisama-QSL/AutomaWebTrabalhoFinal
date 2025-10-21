8e8cc6b8bd6be8fb155fb49ef1239d46885d4555
Elisama <elisamaqs@gmail.com>
2025-10-21 16:01:59 -0300

    Atualização do cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,17 @@ module.exports = defineConfig({
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       return config
-    }
+    },
+    reporter: 'mochawesome',
+    reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
   },
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   viewportWidth: 414,
   viewportHeight: 896,
   defaultCommandTimeout: 10000,
