04a5b818b84714ef39ffc8166c735ecd58034638
Elisama <elisamaqs@gmail.com>
2025-10-21 16:27:19 -0300

    Atualização do cypress config novamente

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,24 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationexercise.com/',
     supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
-      return config
+      return config;
     },
     reporter: 'mochawesome',
     reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: true,
-    json: true,
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+    video: true,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    viewportWidth: 414,
+    viewportHeight: 896,
+    defaultCommandTimeout: 10000,
   },
-  video: true,
-  screenshotsFolder: 'cypress/screenshots',
-  videosFolder: 'cypress/videos',
-  viewportWidth: 414,
-  viewportHeight: 896,
-  defaultCommandTimeout: 10000,
-})
+});
